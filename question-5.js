3227c8f289277f11154f57405c28d13e1630c7fc
Pond <pondwachi.th@gmail.com>
2024-07-30 10:46:08 +0700

    feat: algo cheeck point

--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer:
+Answer:O(m**2) เพราะว่ามีการใช้ loop ซ้อนเข้าไปใน loop อีกที ซึ่งloopแรงจะloop เช็คข้อมูลและ loop ที่ 2 จะทำการจัดเรียงข้อมูลใหม่ตามลำดับไปพร้อมกับ loop แรก 
 
 
 */
